40b63c66cce7796fa3bf964ff1ec6d75d9eb4c86
dbrsseau <dbrsseau@gmail.com>
2021-01-09 03:37:24 -0500

    Add user input validation

---
+++ src/com/rna/search/RNAEngine.java
@@ -0,0 +1,23 @@
+package com.rna.search;
+
+import java.util.ArrayList;
+
+/**
+ * Ribonucleic acid engine
+ * @author  David Brousseau
+ * @version 0.1
+ */
+public class RNAEngine {
+
+    /** Ribonucleic acid */
+    private final ArrayList<Codon> rna;
+
+    /**
+     * Constructor
+     * @param input User input
+     */
+    public RNAEngine(String input) {
+        this.rna = new ArrayList<>();
+        // TODO:
+    }
+}
--- src/com/rna/search/Terminal.java
+++ src/com/rna/search/Terminal.java
@@ -1,6 +1,5 @@
 package com.rna.search;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -10,18 +9,70 @@ import java.util.Scanner;
  */
 public class Terminal {
 
-    /** Ribonucleic acid */
-    private final ArrayList<Codon> rna;
-
+    /** Ribonucleic acid engine */
+    private final RNAEngine engine;
     /** Input scanner */
-    private final Scanner scanner;
+    private final Scanner   scanner;
 
     /** Constructor */
     private Terminal() {
-        this.rna = new ArrayList<>();
-        this.scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
+        final String userInput = readUserInput();
+        validateUserInput(userInput);
+        engine = new RNAEngine(userInput);
         // TODO:
-        this.scanner.close();
+        scanner.close();
+    }
+
+    /**
+     * Check if the user input characters are A, C, G or U.
+     * @param  input User input
+     * @return       True of False
+     */
+    private boolean isACGU(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != 'A' && input.charAt(i) != 'C' && input.charAt(i) != 'G' && input.charAt(i) != 'U') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check if the user input length is greater than 5.
+     * @param  input User input
+     * @return       True of False
+     */
+    private boolean isGreaterThan5(String input) {
+        if (input.length() > 5) { return true; }
+        return false;
+    }
+
+    /**
+     * Check if the user input length is a multiple of 3.
+     * @param  input User input
+     * @return       True of False
+     */
+    private boolean isMultiple3(String input) {
+        if (input.length() % 3 == 0) { return true; }
+        return false;
+    }
+
+    private String readUserInput() {
+        System.out.println("Entrer votre ARN :");
+        final String userInput = scanner.nextLine();
+        return userInput;
+    }
+
+    /**
+     * Validate the user input format.
+     * @param input User input
+     */
+    private void validateUserInput(String input) {
+        if (!(isACGU(input) && isGreaterThan5(input) && isMultiple3(input))) {
+            System.err.println("Votre ARN est invalide");
+            System.exit(1);
+        }
     }
 
     /**
